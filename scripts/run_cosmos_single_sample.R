d4e69bc4bc769c14f53c75c505d3805a462b89b1
DustinSchilling <82500269+DustinSchilling@users.noreply.github.com>
2022-05-06 18:44:40 +0200

    implemented single patient run

--- scripts/run_cosmos_single_sample.R
+++ scripts/run_cosmos_single_sample.R
@@ -43,35 +43,66 @@ prot_cosmos <- prot_cosmos %>%
 Biocrates_metabolite_identifier <- read_excel("support/Biocrates_metabolite_identifier.xlsx")
 Biocrates_metabolite_identifier$feature <- gsub("[()/: -]",".",Biocrates_metabolite_identifier$feature)
 
-met_cosmos1 <- merge(met_cosmos, Biocrates_metabolite_identifier, by.x = "ID", by.y = "feature")
-# why less -> x1.Met.His and X3.Met.His left out
-
-
-
-
+met_cosmos <- merge(met_cosmos, Biocrates_metabolite_identifier, by.x = "ID", by.y = "feature")
+# why less -> x1.Met.His and X3.Met.His left out for 1FF2F9. Is this a problem?
 
 met_cosmos <- met_cosmos %>%
   mutate(HMDB = strsplit(as.character(HMDB), "/")) %>%
   unnest(HMDB) %>%
-  filter(HMDB != "") 
-
-
-
-
+  filter(HMDB != "")
 
 mapping <- met_cosmos[,c(1,6)]
 
-cosmos_met_input <- unlist(met_cosmos[,which(colnames(met_cosmos) == patient)])
+cosmos_met_input <- unlist(met_cosmos$logFC_z)
 names(cosmos_met_input) <- met_cosmos$HMDB
 
+cosmos_met_input <- prepare_metab_inputs(cosmos_met_input, c("c","m"))
 
-prot_cosmos <- prot_cosmos %>%
-  mutate(ID = strsplit(as.character(ID), ";")) %>%
-  unnest(ID) %>%
-  filter(ID != "")
+cosmos_prot_input <- unlist(prot_cosmos$logFC_z)
+names(cosmos_prot_input) <- prot_cosmos$ID
+
+#In order to adapt options to users specification we can load them into a variable 
+#that will then be passed to preprocess_COSMOS_signaling_to_metabolism CARNIVAL_options parameter
+my_options <- default_CARNIVAL_options(solver = "cplex")
+
+#Here the user should provide a path to its CPLEX executable (only cplex at the moment, other solvers will be documented soon !)
+# my_options$solverPath <- "~/Documents/cplex" #or cbc solver executable
+my_options$solverPath <- "cplex/cplex.exe"
+# my_options$solverPath <- "cbc/cbc-osx/cbc" #or cbc solver executable
+my_options$solver <- "cplex" #or cbc
+# my_options$solver <- "cbc"
+my_options$timelimit <- 1800
+my_options$mipGAP <- 0.05
+my_options$threads <- 6
+
+metab_input <- cosmosR:::filter_input_nodes_not_in_pkn(cosmos_met_input, meta_network)
+sig_input <- cosmosR:::filter_input_nodes_not_in_pkn(cosmos_prot_input, meta_network)
+
+test_for <- preprocess_COSMOS_signaling_to_metabolism(meta_network = meta_network,
+                                                      signaling_data = sig_input,
+                                                      metabolic_data = metab_input,
+                                                      maximum_network_depth = 4,
+                                                      CARNIVAL_options = my_options)
+
+
+my_options$timelimit <- 600
+
+test_result_for <- run_COSMOS_signaling_to_metabolism(data = test_for,
+                                                      CARNIVAL_options = my_options)
+
+formatted_res <- format_COSMOS_res(test_result_for)
+
+SIF <- formatted_res[[1]]
+ATT <- formatted_res[[2]]
+
+SIF <- SIF[which(SIF$Weight != 0),]
+
+ATT <- merge(ATT, proteomics_DE_t[,c(3,6)], all.x = T, by.x = "Nodes", by.y = "ID")
+ATT$Nodes <- gsub(",","_",ATT$Nodes)
 
 
 
 
-write_csv(SIF, file = paste("results/",paste(patient, "_SIF.csv",sep = ""), sep = ""))
-write_csv(ATT, file = paste("results/",paste(patient, "_ATT.csv",sep = ""), sep = ""))
+
+write_csv(SIF, file = paste("results/single_patient/",paste(patient, "_SIF.csv",sep = ""), sep = ""))
+write_csv(ATT, file = paste("results/single_patient/",paste(patient, "_ATT.csv",sep = ""), sep = ""))
