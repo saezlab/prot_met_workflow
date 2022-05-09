a7235713c4eda156f79596fb13d70a513f051198
DustinSchilling <82500269+DustinSchilling@users.noreply.github.com>
2022-05-09 23:05:06 +0200

    save logFC_z-score and start of decoupleR

--- scripts/preparation_sample_analysis.R
+++ scripts/preparation_sample_analysis.R
@@ -17,7 +17,6 @@ raw_prot <- readRDS("data/SummarizedExperiment_extraction_method_cohort_proteomi
 # raw_metab
 metab_count <- as.data.frame(raw_metab@assays@data@listData[[1]])
 colnames(metab_count) <- raw_metab@colData@listData[["Sample.Description"]]
-metab_count <- tibble::rownames_to_column(metab_count, "ID")
 
 metab_count <- unique(metab_count)
 metabolomics_TUvsNG <- as.data.frame(metab_count)
@@ -37,7 +36,8 @@ prot_count <- prot_count %>%
 prot_count <- prot_count[prot_count$ID != "NA",]
 prot_count <- unique(prot_count)
 
-proteomics_TUvsNG <- as.data.frame(prot_count)
+proteomics_TUvsNG <- as.data.frame(prot_count[,-1])
+rownames(proteomics_TUvsNG) <- prot_count$ID
 
 # additionally differentiate between autoSP3 and MTBE
 autoSP3orMTBE <- raw_prot@colData@listData[["Processing"]]
@@ -46,10 +46,10 @@ sum(autoSP3orMTBE == "MTBE_SP3")
 # some samples do not have healthy or cancerous partner with sample preparation
 # Thereby, these will be combined so it ends up with 20 samples that are pair-wise combined just like in metabolomics 
 # We will do these manually so it is exactly the same as metabolomics
-proteomics_TUvsNG <- proteomics_TUvsNG[,c(1, 7, 6, 13, 12, 17, 16, 15, 14, 3, 2, 28, 27, 25, 24, 19, 18, 10, 9, 5, 4)]
+proteomics_TUvsNG <- proteomics_TUvsNG[,c(6, 5, 12, 11, 16, 15, 14, 13, 2, 1, 27, 26, 24, 23, 18, 17, 9, 8, 4, 3)]
 # "IJ17TV_NG"  "IJ17TV_TU" "8U04FR_NG"  "8U04FR_TU" these samples are from autoSP3 and not MTBE_SP3
 colnames(proteomics_TUvsNG) <- gsub("[.]1", "", colnames(proteomics_TUvsNG))
-colnames(proteomics_TUvsNG)[2] <- "8JDLQY_TU"
+colnames(proteomics_TUvsNG)[1] <- "8JDLQY_TU"
 
 # save raw matrices
 saveRDS(metabolomics_TUvsNG, "./results/metabolomics_TUvsNG.Rda")
@@ -59,20 +59,16 @@ saveRDS(proteomics_TUvsNG, "./results/proteomics_TUvsNG.Rda")
 # log-FC
 
 # metab
-metabolomics_NG <- metabolomics_TUvsNG[,seq(3, 21, by = 2)]
-metabolomics_TU <- metabolomics_TUvsNG[,seq(2, 21, by = 2)]
+metabolomics_NG <- metabolomics_TUvsNG[,seq(2, 20, by = 2)]
+metabolomics_TU <- metabolomics_TUvsNG[,seq(1, 20, by = 2)]
 logFC_metabolomics_TUvsNG <- metabolomics_TU - metabolomics_NG
 colnames(logFC_metabolomics_TUvsNG) <- gsub("_TU", "", colnames(logFC_metabolomics_TUvsNG))
-logFC_metabolomics_TUvsNG <- cbind(metabolomics_TUvsNG[,1], logFC_metabolomics_TUvsNG)
-colnames(logFC_metabolomics_TUvsNG)[1] <- "ID"
 
 # prot
-proteomics_NG <- proteomics_TUvsNG[,seq(3, 21, by = 2)]
-proteomics_TU <- proteomics_TUvsNG[,seq(2, 21, by = 2)]
+proteomics_NG <- proteomics_TUvsNG[,seq(2, 20, by = 2)]
+proteomics_TU <- proteomics_TUvsNG[,seq(1, 20, by = 2)]
 logFC_proteomics_TUvsNG <- proteomics_TU - proteomics_NG
 colnames(logFC_proteomics_TUvsNG) <- gsub("_TU", "", colnames(logFC_proteomics_TUvsNG))
-logFC_proteomics_TUvsNG <- cbind(proteomics_TUvsNG$ID, logFC_proteomics_TUvsNG)
-colnames(logFC_proteomics_TUvsNG)[1] <- "ID"
 
 
 ## z-transformation
@@ -80,17 +76,14 @@ colnames(logFC_proteomics_TUvsNG)[1] <- "ID"
 mean_metabolomics_FC <- rowMeans(as.matrix(logFC_metabolomics_TUvsNG), na.rm = T)
 sd_metabolomics_FC <- rowSds(as.matrix(logFC_metabolomics_TUvsNG), na.rm = T)
 logFC_metabolomics_z <- as.data.frame((logFC_metabolomics_TUvsNG - mean_metabolomics_FC)/sd_metabolomics_FC)
-logFC_metabolomics_z <- cbind(metabolomics_TUvsNG$ID, logFC_metabolomics_z)
-colnames(logFC_metabolomics_z)[1] <- "ID"
 
 #prot
 mean_proteomics_FC <- rowMeans(as.matrix(logFC_proteomics_TUvsNG), na.rm = T)
 sd_proteomics_FC <- rowSds(as.matrix(logFC_proteomics_TUvsNG), na.rm = T)
 logFC_proteomics_z <- as.data.frame((logFC_proteomics_TUvsNG - mean_proteomics_FC)/sd_proteomics_FC)
-logFC_proteomics_z <- cbind(proteomics_TUvsNG$ID, logFC_proteomics_z)
-colnames(logFC_proteomics_z)[1] <- "ID"
 
-
-
+# save
+saveRDS(logFC_metabolomics_z, "./results/logFC_metabolomics_z.Rda")
+saveRDS(logFC_proteomics_z, "./results/logFC_proteomics_z.Rda")
 
 
